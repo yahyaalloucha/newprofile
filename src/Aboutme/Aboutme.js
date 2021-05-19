9dcdbce2aa02cfb70ddfe8681a6ecbcb5006fcc7
Yahya <yahyaallouch01@gmail.com>
2021-05-19 18:28:36 +0300

    final.ver.with.e.thing

--- src/Aboutme/Aboutme.js
+++ src/Aboutme/Aboutme.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Component } from "react";
 
-import { BrowserRouter as Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Contact from "../Contact/Contact";
 
 import Styles from "./Aboutme.module.css";
